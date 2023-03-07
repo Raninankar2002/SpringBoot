015470dce231c5d3f80fcba132c05e2f815f90bc
Raninankar2002 <96129980+Raninankar2002@users.noreply.github.com>
2023-03-07 20:58:11 +0530

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,40 @@
+package com.simplecalculator.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/calculator/add")
+	public String calculate(@RequestParam(value="n1",required=true,defaultValue="1")int n1,
+			@RequestParam(value="n2",required=true,defaultValue="2")int n2)//It is a calculate method is use for the take parameter
+	{
+		
+		return "<h1> The sum is "+CalculatorLogic.addNo(n1, n2)+"</h1>";//call CalculatorLogic class addNo() method
+	}
+	@GetMapping("/calculator/sub")
+	public String calculate1(@RequestParam(value="n1",required=true,defaultValue="1")int n1,
+			@RequestParam(value="n2",required=true,defaultValue="2")int n2)//It is a calculate1 method is use for the take parameter
+	{
+		
+		return "<h1> The substraction is "+CalculatorLogic.subNo(n1, n2)+"</h1>";//call CalculatorLogic class subNo() method
+	}
+	@GetMapping("/calculator/mul")
+	public String calculate2(@RequestParam(value="n1",required=true,defaultValue="1")int n1,
+			@RequestParam(value="n2",required=true,defaultValue="2")int n2)//It is a calculate2 method is use for the take parameter
+	{
+		
+		return "<h1> The multiplication is "+CalculatorLogic.mulNo(n1, n2)+"</h1>";//call CalculatorLogic class mulNo() method
+	}
+	@GetMapping("/calculator/div")
+	public String calculate3(@RequestParam(value="n1",required=true,defaultValue="1")int n1,
+			@RequestParam(value="n2",required=true,defaultValue="2")int n2)//It is a calculate3 method is use for the take parameter
+	{
+		
+		return "<h1> The Division is "+CalculatorLogic.divNo(n1, n2)+"</h1>";//call CalculatorLogic class divNo() method
+	}
+
+}
